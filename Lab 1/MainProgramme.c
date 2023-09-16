b4b1ce4b557d87296e35e8220c44d63eb12e8ebb
Dennis Chen <dennischencheehaodcch@gmail.com>
2023-09-16 12:52:40 +0800

    Done up analysis of merge insertion sort

--- Lab 1/MainProgramme.c
+++ Lab 1/MainProgramme.c
@@ -17,7 +17,9 @@ int main()
 {
     srand(time(NULL));
 
-    int arrSize[] = {1000, 2500, 5000, 7500, 10000, 25000, 50000, 75000, 1000000, 2500000, 5000000, 7500000, 10000000};
+    clock_t start_time, end_time;
+
+    int arrSize[] = {10000000};
     int temp1 = sizeof(arrSize) / sizeof(int);
 
     int threshold[] = {2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
@@ -25,10 +27,13 @@ int main()
 
     int* arr, *tempArr;
     int arraySize;
-    int results[13][5][19];
+    //int results[1][5][20];
+    double time[1][5][1];
+    double mergeTime[5];
 
-    FILE * fpointer = fopen("results_c2_1.csv", "w");
-    fprintf(fpointer, "Array Size[N],Threshold[S],Key Comparison 1, Key Comparison 2, Key Comparison 3, Key Comparison 4, Key Comparison 5\n");
+    FILE * fpointer = fopen("results_d.csv", "a");
+    //fprintf(fpointer, "Array Size[N],Threshold[S],Key Comparison 1, Key Comparison 2, Key Comparison 3, Key Comparison 4, Key Comparison 5, Time 1, Time 2, Time 3, Time 4, Time 5\n");
+    //fprintf(fpointer, "Array Size[N],Threshold[S], Time 1, Time 2, Time 3, Time 4, Time 5\n");
 
     //For different Array Size
     for(int i = 0; i < temp1; i++){
@@ -41,6 +46,13 @@ int main()
             //Make new array every new experiment
             arr = generateArray(arraySize);
 
+            //traditional merge sort
+            //start_time = clock();
+            //mergeSort(arr, 0, arraySize - 1);
+            //end_time = clock();
+
+            //mergeTime[j] = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
+
             //For different threshold level
             for(int k = 0; k < temp2; k++){
 
@@ -50,11 +62,14 @@ int main()
                 //Duplicate Array for each threshold
                 tempArr = duplicateArray(arr, arraySize);
 
-                //Do merge insertion sort
+                //Do merge insertion sort with time
+                start_time = clock();
                 hybridSort(tempArr, 0, arraySize -  1, threshold[k]);
+                end_time = clock();
 
                 //Store results
-                results[i][j][k] = keyComparison;
+                //results[i][j][k] = keyComparison;
+                time[i][j][k] = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
 
                 free(tempArr);
             }
@@ -64,10 +79,23 @@ int main()
     }
 
     //Write to csv file
+    /*
     for(int i = 0; i < temp1; i++){
         for(int j = 0; j < temp2; j++)
-            fprintf(fpointer, "%d,%d,%d,%d,%d,%d,%d\n", arrSize[i], threshold[j], results[i][0][j], results[i][1][j], results[i][2][j], results[i][3][j], results[i][4][j]);
+            fprintf(fpointer, "%d,%d,%d,%d,%d,%d,%d\n", arrSize[i], threshold[j],
+                    results[i][0][j], results[i][1][j], results[i][2][j], results[i][3][j], results[i][4][j]);
     }
+    */
+
+    fprintf(fpointer, "%d, Traditional Merge,%f,%f,%f,%f,%f\n", arrSize[0], mergeTime[0], mergeTime[1], mergeTime[2], mergeTime[3], mergeTime[4]);
+
+    for(int i = 0; i < temp1; i++){
+        for(int j = 0; j < temp2; j++)
+            fprintf(fpointer, "%d,%d,%f,%f,%f,%f,%f\n", arrSize[i], threshold[j],
+                    time[i][0][j], time[i][1][j], time[i][2][j], time[i][3][j], time[i][4][j]);
+    }
+
+
 
 
     fclose(fpointer);
@@ -149,6 +177,22 @@ void merge(int arr[], int left, int mid, int right)
 
     free(tempLeftArray);
     free(tempRightArray);
+}
+
+void mergeSort(int arr[], int left, int right)
+{
+    if(right - left > 0){
+        int mid  = (left + right) / 2;
+
+        //recursive
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        //combine the 2 sub arrays
+        merge(arr, left, mid, right);
+    }
+    else
+        return;
 }
 
 //Array Generator
